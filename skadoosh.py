1637a3a96fb8bfa013dadd33a041ec2e45ee6e96
Vivek Alamuri <61337145+valamuri2020@users.noreply.github.com>
2021-11-02 21:18:43 -0400

    speech to text enabled when calling sign is made

--- skadoosh.py
+++ skadoosh.py
@@ -1,22 +1,33 @@
+# TODO: reorder imports
+# TODO: add page scrolling
+# TODO: fix audio input screen freeze
+
 import cv2
 import numpy as np 
+import keyboard
 import mouse
-from screeninfo import get_monitors
-import hand_tracking_module as htm
-from colors import colors
 import math 
+from screeninfo import get_monitors
+
+from colors import colors
+import hand_tracking_module as htm
+
 from ctypes import cast, POINTER
 from comtypes import CLSCTX_ALL
 from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
-# read in webcam input and get screen dimensions
+
+import speech_detector as sr
+
 cap = cv2.VideoCapture(0)
 
+# webcam settings
 camWidth, camHeight = 640, 480
 scrWidth, scrHeight = get_monitors()[0].width, get_monitors()[0].height 
 # 1920, 1080
 cap.set(3, camWidth)
 cap.set(4, camHeight)
 
+# hand detector variables
 detector = htm.HandDetector(detectionConfidence=0.85, maxHands=1)
 smootheningFactor = 7
 prevX, prevY = 0, 0
@@ -34,6 +45,9 @@ barVol = 400
 vol = 0
 volPercent = 0
 
+# speech recognition listener
+listener = sr.SpeechDetector()
+
 while True:
     success, img = cap.read()
     
@@ -48,13 +62,13 @@ while True:
 
         # returns a list of fingers that are open; thumb is at 0th index 
         fingers = detector.fingersOpen()
-        
-        # reduce virtual mousepad area, then draw
+        # constant to reduce mousepad size
         frameR = 120
-        cv2.rectangle(img, (frameR, frameR), (camWidth - frameR, camHeight - frameR), colors["purple"], 2)
         
         # when middle, ring, and pinky fingers closed
-        if sum(fingers[2:]) == 0:        
+        if sum(fingers[2:]) == 0:     
+            # reduce virtual mousepad area, then draw
+            cv2.rectangle(img, (frameR, frameR), (camWidth - frameR, camHeight - frameR), colors["purple"], 2)   
             # convert camera coordinates to screen coordinates
             x3 = np.interp(x1, (frameR, camWidth - frameR), (0, scrWidth))
             y3 = np.interp(y1, (frameR, camHeight - frameR), (0, scrHeight))
@@ -69,30 +83,27 @@ while True:
             prevX, prevY = currX, currY
         
         # if index and middle fingers are open
-        if fingers[1] == 1 and fingers[2] == 1:        
+        if fingers[1] == 1 and fingers[2] == 1:  
+            # draw mousepad
+            cv2.rectangle(img, (frameR, frameR), (camWidth - frameR, camHeight - frameR), colors["purple"], 2)
+                  
             length, img, lineInfo = detector.calculateDistance(8, 12, img)
             # center of line connecting index and middle fingers
             cLineX, cLineY = lineInfo[4], lineInfo[5]
             if length < 16:
                 cv2.circle(img, (cLineX, cLineY), 15, colors["green"], cv2.FILLED)
                 mouse.click()
-        
-        
+               
     """ Gesture Control Volume """
     if len(landmarks) != 0: 
-        x1, y1 = landmarks[4][1], landmarks[4][2]
-        x2, y2 = landmarks[8][1], landmarks[8][2]
+        x1, y1 = landmarks[4][1:]
+        x2, y2 = landmarks[8][1:]
         # center of line
         cx, cy = (x1+x2) // 2, (y1+y2)//2
-
-        cv2.circle(img, (x1,y1), 12, colors["purple"], cv2.FILLED)
-        cv2.circle(img, (x2,y2), 12, colors["purple"], cv2.FILLED)
-        cv2.circle(img, (cx,cy), 12, colors["purple"], cv2.FILLED)
-        cv2.line(img, (x1, y1), (x2,y2), colors["purple"], thickness=3)
         
         length = math.hypot(x2-x1, y2-y1)
-        # hand range is 20 - 180
-        # vol range is -65.25 - 0
+        # hand range: 20 - 180
+        # vol range: -65.25 - 0
         vol = np.interp(length, [20,125], [volMin, volMax])
         barVol = np.interp(vol, [20, 125], [400,150])
         volPercent = np.interp(vol, [20, 125], [0, 100])
@@ -101,12 +112,41 @@ while True:
         
         # volume changing activated when pinky down and other fingers open so changes do not occur when a fist is made
         if not(fingers[4]) and sum(fingers[1:4]) == 3:
+            cv2.circle(img, (x1,y1), 12, colors["purple"], cv2.FILLED)
+            cv2.circle(img, (x2,y2), 12, colors["purple"], cv2.FILLED)
+            cv2.circle(img, (cx,cy), 12, colors["purple"], cv2.FILLED)
+            cv2.line(img, (x1, y1), (x2,y2), colors["purple"], thickness=3)
+            
             volume.SetMasterVolumeLevel(vol, None)
             cv2.line(img, (x1, y1), (x2,y2), colors["blue"], thickness=3)    
+        
         if length < 20:
             cv2.circle(img, (cx,cy), 12, colors["blue"], cv2.FILLED)
     
+    """ Speech to text """
+    if len(landmarks) != 0:
+        fingers = detector.fingersOpen()
+        
+        x1, y1 = landmarks[4][1:]
+        x2, y2 = landmarks[20][1:]
+        
+        if sum(fingers[1:4]) == 0 and fingers[4]:
+            cv2.putText(img, "Listening...", (camWidth//2 - 30, camHeight), cv2.FONT_HERSHEY_PLAIN, 2, colors["blue"], 2)
+            cv2.circle(img, (x1,y1), 12, colors["red"], cv2.FILLED)
+            cv2.circle(img, (x2,y2), 12, colors["red"], cv2.FILLED)
+            
+            print("speech to text mode")
+            text = listener.getAudio()
+            print("received :", text)
+            if type(text) != None :
+                keyboard.write(text=text)
+                
 
-    cv2.imshow("img", img)
-    cv2.waitKey(1)
+
+    cv2.imshow("skadoosh ✨", img)
     
+    if cv2.waitKey(1) & 0xFF == ord('d'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
---
+++ speech_detector.py
@@ -0,0 +1,18 @@
+import speech_recognition as sr
+
+class SpeechDetector():
+    def __init__(self) -> None:
+        self.listener = sr.Recognizer()
+    
+    def getAudio(self):
+        with sr.Microphone() as source:
+            try:
+                print("listening...")
+                voice = self.listener.listen(source)
+                data = self.listener.recognize_google(voice)
+                print(data)
+                return data
+            except sr.UnknownValueError:
+                print("Google Speech Recognition could not understand audio")
+            except sr.RequestError as e:
+                print(f"Could not request results from Google Speech Recognition service; {e}")
